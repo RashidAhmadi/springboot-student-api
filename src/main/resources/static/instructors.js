a7eea0a7ff77a53eb818babb64941bec9f91f7c2
Rashid <rashid.angury20@gmail.com>
2025-11-25 22:50:01 +0100

    CSS style to instructor page applied

--- src/main/resources/static/instructors.js
+++ src/main/resources/static/instructors.js
@@ -57,39 +57,9 @@ function loadInstructors() {
                         <td>${ins.degree}</td>
                         <td>${ins.employment}</td>
                         <td>${ins.proficiency}</td>
-                        <td>
-                            <button onclick="editInstructor(${ins.id})">Edit</button>
-                            <button onclick="deleteInstructor(${ins.id})">Delete</button>
-                        </td>
-                    </tr>
-                `;
-            });
-        })
-        .catch(err => console.error("Error loading instructors:", err));
-}
-
-function loadInstructors() {
-    fetch("http://localhost:8080/api/instructors/all")
-        .then(response => response.json())
-        .then(data => {
-            const tbody = document.getElementById("instructorTableBody");
-            tbody.innerHTML = "";
-
-            data.forEach((ins, index) => {
-                tbody.innerHTML += `
-                    <tr>
-                        <td>${index + 1}</td>
-                        <td>${ins.name}</td>
-                        <td>${ins.lastname}</td>
-                        <td>${ins.faculty}</td>
-                        <td>${ins.department}</td>
-                        <td>${ins.academicRank}</td>
-                        <td>${ins.degree}</td>
-                        <td>${ins.employment}</td>
-                        <td>${ins.proficiency}</td>
-                        <td>
-                            <button onclick="editInstructor(${ins.id})">Edit</button>
-                            <button onclick="deleteInstructor(${ins.id})">Delete</button>
+                        <td class="actions">
+                            <button class="btn-edit" onclick="editInstructor(${ins.id})">Edit</button>
+                            <button class="btn-delete" onclick="deleteInstructor(${ins.id})">Delete</button>
                         </td>
                     </tr>
                 `;
